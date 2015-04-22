597db8fb694b4100435798dc96ac2c6da738e765
yotten <yakihiro@ac.auone-net.jp>
2015-04-23 01:36:03 +0900

    ルーチンの移動

--- customer.rb
+++ customer.rb
@@ -17,7 +17,7 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      this_amount = amount_for(element)
+      this_amount = element.charge
 
       frequent_renter_points += 1
       if (element.movie.price_code == Movie::NEW_RELEASE) && (element.days_rented > 1)
@@ -31,23 +31,5 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result 
-  end
-
-  def amount_for(rental)
-    ret = 0 
-
-    case rental.movie.price_code
-    when Movie::REGULAR
-      ret += 2
-      ret += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      ret += rental.days_rented * 3
-    when Movie::CHILDRENS
-      ret += 1.5
-      ret += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      ret =ret.to_i if ret == ret.to_i
-    end
-
-    ret 
   end
 end
--- rental.rb
+++ rental.rb
@@ -3,5 +3,23 @@ class Rental
 
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented  
-  end  
+  end 
+
+  def charge
+    ret = 0 
+
+    case movie.price_code
+    when Movie::REGULAR
+      ret += 2
+      ret += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      ret += days_rented * 3
+    when Movie::CHILDRENS
+      ret += 1.5
+      ret += (days_rented - 3) * 1.5 if days_rented > 3
+      ret =ret.to_i if ret == ret.to_i
+    end
+
+    ret
+  end
 end
