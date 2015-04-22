85b4c79f1e9e988447ef61c41b2b8b642e5fd2f2
yotten <yakihiro@ac.auone-net.jp>
2015-04-23 01:25:37 +0900

    メソッドの抽出

--- customer.rb
+++ customer.rb
@@ -17,19 +17,7 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      this_amount = 0
-
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-        this_amount = this_amount.to_i if this_amount == this_amount.to_i 
-      end
+      this_amount = amount_for(element)
 
       frequent_renter_points += 1
       if (element.movie.price_code == Movie::NEW_RELEASE) && (element.days_rented > 1)
@@ -43,5 +31,23 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result 
+  end
+
+  def amount_for(element)
+    this_amount = 0 
+
+    case element.movie.price_code
+    when Movie::REGULAR
+      this_amount += 2
+      this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+    when Movie::NEW_RELEASE
+      this_amount += element.days_rented * 3
+    when Movie::CHILDRENS
+      this_amount += 1.5
+      this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      this_amount = this_amount.to_i if this_amount == this_amount.to_i
+    end
+
+    this_amount
   end
 end
